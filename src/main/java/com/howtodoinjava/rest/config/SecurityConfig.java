01363a7a7daaaf6b5a67b9c61c49f8eeedfe3a24
Fabrice LANAI <flanai@sigma.fr>
2020-03-09 08:50:36 +0100

    first commit

---
+++ src/main/java/com/howtodoinjava/rest/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.howtodoinjava.rest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+
+        http.authorizeRequests()
+                .antMatchers("/flow").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+        //.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").invalidateHttpSession(true)        // set invalidation state when logout
+        //      .deleteCookies("JSESSIONID") ;
+
+        /*http
+         .csrf().disable()
+         .authorizeRequests().anyRequest().authenticated()
+         .and()
+         .httpBasic();*/
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}pass").roles("USER");
+    }
+
+
+    /*
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("pass").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("vaquar").password("pass").roles("USER");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/**").hasAnyRole("ADMIN","USER").and().httpBasic();//.and().headers().disable();
+        //.and().formLogin();
+    }
+}
+     */
+}
---
+++ src/main/java/com/howtodoinjava/rest/controller/Controller.java
@@ -0,0 +1,98 @@
+package com.howtodoinjava.rest.controller;
+
+import java.net.URI;
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.howtodoinjava.rest.dao.EmployeeDAO;
+import com.howtodoinjava.rest.model.Employee;
+import com.howtodoinjava.rest.model.Employees;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping(path = "/")
+public class Controller
+{
+    /*@Autowired
+    private EmployeeDAO employeeDao;
+    
+    @GetMapping(path="/", produces = "application/json")
+    public Employees getEmployees() 
+    {
+        return employeeDao.getAllEmployees();
+    }
+    
+    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(
+        @RequestHeader(name = "X-COM-PERSIST", required = true) String headerPersist,
+        @RequestHeader(name = "X-COM-LOCATION", defaultValue = "ASIA") String headerLocation,
+        @RequestBody Employee employee) 
+            throws Exception 
+    {       
+        //Generate resource id
+        Integer id = employeeDao.getAllEmployees().getEmployeeList().size() + 1;
+        employee.setId(id);
+
+        //add resource
+        employeeDao.addEmployee(employee);
+
+        //Create resource location
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                    .path("/{id}")
+                                    .buildAndExpand(employee.getId())
+                                    .toUri();
+
+        //Send location in response
+        return ResponseEntity.created(location).build();
+    }*/
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request)  {
+        HttpSession session=request.getSession();
+        System.out.println("session id before invalidating session is:"+session.getId());
+        session.invalidate();
+       System.out.println("session id after invalidating session is:"+session.getId());
+
+
+        return "logoutt done";
+    }
+
+    @RequestMapping("/")
+    public String index()  {
+
+        return "<a href='/logout'>logout</a><br />" + "<a href='/clear'>clear</a>";
+    }
+
+    @RequestMapping(value = {"/clear"})
+    public String clear(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session= request.getSession(false);
+        SecurityContextHolder.clearContext();
+        session= request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+        for(javax.servlet.http.Cookie cookie : request.getCookies()) {
+            cookie.setMaxAge(0);
+        }
+
+        return "logout";
+    }
+
+    @RequestMapping(value = {"/flow"})
+    public String flow(){
+        return "flow without auth...";
+    }
+}
